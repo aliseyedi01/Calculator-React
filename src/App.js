21e6efde30db96ece8eafc1157b38f932d704ddd
aliseyedi <solafiali657@gmail.com>
2023-03-05 18:47:16 +0330

    add component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,26 @@
 import "./App.css";
 import { useState } from "react";
-import Title from "./components/Title";
+import Calculator from "./components/Calculator.js";
 
 function App() {
-  const [showEvents, setShowEvents] = useState(true);
+  const [expression, setExpression] = useState("");
+  const [display, setDisplay] = useState("");
 
-  const [events, setEvents] = useState(
-    { title: "THOR : love and thunder ", id: 1 },
-    { title: "THOR : Books and Pens ", id: 2 },
-    { title: "THOR : Home and Car ", id: 3 },
-    { title: "THOR : Mobile and Tablets ", id: 4 },
-    { title: "THOR : Iran and America ", id: 1 }
-  );
+  function handleClick(value) {
+    setDisplay(value);
+    setExpression([...expression, value]);
+  }
 
-  const handleClick = (id) => {
-    setEvents((prevEvents) => {
-      return prevEvents.filter((event) => {
-        return id !== event.id;
-      });
-    });
-  };
+  function getResult() {
+    setDisplay(eval(expression));
+    setExpression(" ");
+  }
 
   return (
     <div className="App">
-      <Title />;
-      {showEvents && (
-        <div>
-          <button onClick={() => setEvents(false)}>Hide</button>
-        </div>
-      )}
-      {!showEvents && (
-        <div>
-          <button onClick={() => setEvents(true)}>Show</button>
-        </div>
-      )}
-      {showEvents &&
-        events.map((event, index) => {
-          <div key={event.id}>
-            <h2>
-              {index + 1} - {event.title}
-            </h2>
-            <button
-              onClick={() => {
-                handleClick(event.id);
-              }}
-            >
-              Delete
-            </button>
-          </div>;
-        })}
+      <div className="container">
+        <Calculator handleClick={handleClick} getResult={getResult} display={display} expression={expression} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Calculator({ display, expression, handleClick, getResult }) {
+  return (
+    <div className="App">
+      <h3 className="display">{display}</h3>
+
+      <span className="expression">{expression}</span>
+
+      <section className="panel">
+        <section className="numbers">
+          <button onClick={() => handleClick(7)}>7</button>
+          <button onClick={() => handleClick(8)}>8</button>
+          <button onClick={() => handleClick(9)}>9</button>
+
+          <button onClick={() => handleClick(4)}>4</button>
+          <button onClick={() => handleClick(5)}>5</button>
+          <button onClick={() => handleClick(6)}>6</button>
+
+          <button onClick={() => handleClick(1)}>1</button>
+          <button onClick={() => handleClick(2)}>2</button>
+          <button onClick={() => handleClick(3)}>3</button>
+
+          <button onClick={() => handleClick(0)}>0</button>
+        </section>
+
+        <section className="operators">
+          <button onClick={() => handleClick("÷")}>÷</button>
+          <button onClick={() => handleClick("x")}>x</button>
+          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("+")}>+</button>
+          <button onClick={() => getResult()}> =</button>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default Calculator;
